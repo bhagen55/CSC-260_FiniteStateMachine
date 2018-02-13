c2b18d9c3c1f1addd6dca21a250ce8ce25abd4c7
Blair Hagen <hagenb@union.edu>
2018-02-13 11:44:47 -0500

    fixed package issues

---
+++ src/main/java/proj2/view/gui/Gui.java
@@ -0,0 +1,18 @@
+package proj2.view.gui;
+
+import javax.swing.SwingUtilities;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import proj2.view.gui.DrawPanel;
+
+/*
+* Main gui class
+* Adapted from this tutorial: https://docs.oracle.com/javase/tutorial/uiswing/painting/refining.html
+*/
+
+public class Gui {
+
+
+
+}
