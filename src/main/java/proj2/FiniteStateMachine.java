77832e814eaa2d293e68b7cfc315cc9e810f2c39
Blair Hagen <hagenb@union.edu>
2018-02-12 20:50:44 -0500

    Added overarching file to represent entire project

---
+++ src/main/java/proj2/FiniteStateMachine.java
@@ -0,0 +1,27 @@
+package proj2;
+
+import java.util.ArrayList;
+
+import proj2.document.Document;
+import proj2.view.Gui;
+import proj2.view.DrawPanel;
+
+/*
+* Encompassing class to hold our implementation of a finite state machine
+*
+* Deals with creation of the GUI and the document as well as allowing them to
+* communicate with each other.
+*
+*/
+public class FiniteStateMachine {
+
+	ArrayList views;
+	Document doc;
+
+	public FiniteStateMachine() {
+
+
+
+	}
+
+}
