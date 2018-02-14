02bbc7d12fe19e5ec6cc8eee0848236f985beb42
Blair Hagen <hagenb@union.edu>
2018-02-14 16:10:57 -0500

    added back testing file that actually is needed

---
+++ src/main/java/proj2/TestingSaveAndLoad.java
@@ -0,0 +1,50 @@
+package proj2;
+
+import proj2.document.Document;
+import java.io.*;
+
+public class TestingSaveAndLoad{
+	//Static Document d;
+	//Static FileFormatController f;
+	public TestingSaveAndLoad()
+	{
+		try{
+		Document d;
+	    FileFormatController f;
+		d= new Document();
+		f= new FileFormatController(d);
+		d.addVertex("A",10,52);
+
+		d.addVertex("B",55,117);
+		d.addVertex("C",1,27);
+		d.addEdge("A","B","z");
+		f.saveFile("FiniteStateMachine");
+
+	}
+		 catch(FileNotFoundException ex) {
+             System.out.println(
+                 "Unable to open file '" +
+                 "FiniteStateMachine" + "'");
+         }
+         catch(IOException ex) {
+             System.out.println(
+                 "Error reading file '"
+                 + "FiniteStateMachine" + "'");
+
+         }
+	}
+
+	public static void main(String[] args) throws FileNotFoundException
+	{
+		Document d;
+	    FileFormatController f;
+		d= new Document();
+		f= new FileFormatController(d);
+		d.addVertex("A");
+		d.addVertex("B");
+		d.addVertex("C");
+		d.addEdge("A","B","z");
+		f.saveFile("FiniteStateMachine");
+
+	}
+}
