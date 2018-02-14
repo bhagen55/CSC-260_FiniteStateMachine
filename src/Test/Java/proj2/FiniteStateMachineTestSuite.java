dcdb1180e086e1cc62f2fcf82450cb7711587696
David <david@Davids-MacBook-Pro.local>
2018-02-13 21:35:59 -0500

    Added Test suites

---
+++ src/Test/Java/proj2/FiniteStateMachineTestSuite.java
@@ -0,0 +1,13 @@
+package proj2;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+    FiniteStateMachineTests
+})
+public class FiniteStateMachineTestSuite
+{ // no implementation needed; above annotations do the work.
+}
---
+++ src/Test/Java/proj2/FiniteStateMachineTests.java
@@ -0,0 +1,40 @@
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+
+
+
+@RunWith(JUnit4.class)
+public class FiniteStateMachineTests
+{
+	
+    
+    
+    @Before
+    public void setUp()
+    {
+        
+    }
+
+    @After
+    public void tearDown()
+    {
+       
+    }
+    
+
+
+
+
+    
+}
+
