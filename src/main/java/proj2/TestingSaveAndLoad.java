d2d09be702b791042b45e98039669ed8bedc1403
David <david@Davids-MacBook-Pro.local>
2018-02-13 19:42:47 -0500

    added main function to FileFormatControler

--- src/main/java/proj2/TestingSaveAndLoad.java
+++ src/main/java/proj2/TestingSaveAndLoad.java
@@ -7,6 +7,19 @@ public class TestingSaveAndLoad{
 	FileFormatController f;
 	public TestingSaveAndLoad()
 	{
+		/*
+		d= new Document();
+		f= new FileFormatController(d);
+		d.addVertex("A");
+		d.addVertex("B");
+		d.addVertex("C");
+		d.addEdge("A","B","z");
+		f.saveFile();
+		*/
+	}
+
+	public static void main(String[] args)
+	{
 		d= new Document();
 		f= new FileFormatController(d);
 		d.addVertex("A");
