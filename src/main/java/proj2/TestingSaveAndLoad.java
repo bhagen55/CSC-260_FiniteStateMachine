6570c032aef3255233c0a62348c9d2c693ee1b97
David <david@Davids-MacBook-Pro.local>
2018-02-13 19:44:25 -0500

    added main function to FileFormatControler

--- src/main/java/proj2/TestingSaveAndLoad.java
+++ src/main/java/proj2/TestingSaveAndLoad.java
@@ -3,8 +3,8 @@ package proj2;
 import proj2.document.Document;
 
 public class TestingSaveAndLoad{
-	Static Document d;
-	Static FileFormatController f;
+	//Static Document d;
+	//Static FileFormatController f;
 	public TestingSaveAndLoad()
 	{
 		/*
@@ -20,6 +20,8 @@ public class TestingSaveAndLoad{
 
 	public static void main(String[] args)
 	{
+		Document d;
+	    FileFormatController f;
 		d= new Document();
 		f= new FileFormatController(d);
 		d.addVertex("A");
