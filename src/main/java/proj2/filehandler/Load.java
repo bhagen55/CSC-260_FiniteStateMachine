6b404a9028bf97d99d05c0e9b8caa89b9b5c0ccc
David <david@Davids-MacBook-Pro.local>
2018-02-28 15:41:40 -0500

    created Save and Load Interfaces

--- src/main/java/proj2/filehandler/Load.java
+++ src/main/java/proj2/filehandler/Load.java
@@ -1,1 +1,7 @@
 package proj2.fileHandler;
+public interface Load{
+
+	public void load(String fileName);
+
+
+}
--- src/main/java/proj2/filehandler/Save.java
+++ src/main/java/proj2/filehandler/Save.java
@@ -1,1 +1,7 @@
 package proj2.filehandler;
+public interface Save{
+
+	public void save(String fileName);
+
+
+}
