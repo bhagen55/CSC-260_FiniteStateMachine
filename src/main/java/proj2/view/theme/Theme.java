f378640ba728a5a1fdd76da5706a9c7f28cf3b05
Blair Hagen <hagenb@union.edu>
2018-03-01 20:31:22 -0500

    Added getter and setters to themes

--- src/main/java/proj2/view/theme/Theme.java
+++ src/main/java/proj2/view/theme/Theme.java
@@ -8,11 +8,114 @@ import java.awt.Shape;
 */
 public class Theme
 {
-    // public Color getBackgroundColor();
-    // public Color getStateColor();
-    // public Color getTransitionColor();
-    // public Color getStartStateColor();
-    //
-    // public Shape getStateShape();
-    // public Shape getTransitionShape();
+
+	private String name;
+
+	private Color stateOutlineColor;
+	private Color stateFillColor;
+	private Color stateTextColor;
+	private Color stateAcceptColor;
+	private Color stateStartColor;
+
+	private Color transLineColor;
+	private Color transTextColor;
+
+	private Color backgroundColor;
+
+	public Theme(String name, Color stateOutlineColor, color stateFillColor,
+				Color stateTextColor, Color stateAcceptColor,
+				Color stateStartColor, Color transLineColor,
+				Color transTextColor, Color backgroundColor)
+	{
+		this.name = name;
+
+		this.stateOutlineColor = stateOutlineColor;
+		this.stateFillColor = stateFillColor;
+		this.stateTextColor = stateTextColor;
+		this.stateAcceptColor = stateAcceptColor;
+		this.stateStartColor = stateStartColor;
+
+		this.transLineColor = transLineColor;
+		this.transTextColor = transTextColor;
+
+		this.backgroundColor = backgroundColor;
+	}
+
+
+	/*
+	* Getters
+	*/
+
+	public String toString() {
+		return name;
+	}
+
+
+	public Color getStateOutlineColor() {
+		return stateOutlineColor;
+	}
+
+	public Color getStateFillColor() {
+		return stateFillColor;
+	}
+
+	public Color getStateTextColor() {
+		return stateTextColor;
+	}
+
+	public Color getStateAcceptColor() {
+		return stateAcceptColor;
+	}
+
+	public Color getStateStartColor() {
+		return stateStartColor;
+	}
+
+	public Color getTransLineColor() {
+		return transLineColor;
+	}
+
+	public Color getTransTextColor() {
+		return transTextColor;
+	}
+
+	public Color getBackgroundColor() {
+		return backgroundColor;
+	}
+
+	/*
+	* Setters
+	*/
+
+	public void setStateOutlineColor(Color color) {
+		stateOutlineColor = color;
+	}
+
+	public void setStateFillColor(Color color) {
+		stateFillColor = color;
+	}
+
+	public void setStateTextColor(Color color) {
+		stateTextColor = color;
+	}
+
+	public void setStateAcceptColor(Color color) {
+		stateAcceptColor = color;
+	}
+
+	public void setStateStartColor(Color color) {
+		stateStartColor = color;
+	}
+
+	public void setTransLineColor(Color color) {
+		transLineColor = color;
+	}
+
+	public void setTransTextColor(Color color) {
+		transTextColor = color;
+	}
+
+	public void setBackgroundColor(Color color) {
+		backgroundColor = color;
+	}
 }
