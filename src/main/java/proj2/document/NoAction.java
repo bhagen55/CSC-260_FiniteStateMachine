9705d8a867e9c273d457bed6798a067cb4d6bb27
silfinm <silfinm@union.edu>
2018-03-07 16:36:56 -0500

    Created a NoAction action

---
+++ src/main/java/proj2/document/NoAction.java
@@ -0,0 +1,16 @@
+package proj2.document;
+
+import proj2.simulator.Action;
+
+public class NoAction implements Action
+{
+    public NoAction()
+    {
+
+    }
+
+    public void execute()
+    {
+        
+    }
+}
