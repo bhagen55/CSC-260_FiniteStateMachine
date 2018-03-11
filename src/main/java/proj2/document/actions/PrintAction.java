58bc33104cded1ac2c736ac21cae835431846ba5
silfinm <silfinm@union.edu>
2018-03-11 19:58:42 -0400

    Added javaDoc comments

--- src/main/java/proj2/document/actions/PrintAction.java
+++ src/main/java/proj2/document/actions/PrintAction.java
@@ -4,16 +4,27 @@ import proj2.document.Action;
 
 public class printAction implements Action
 {
+    /**
+     * Default constructor for the print action.
+     */
     public printAction()
     {
 
     }
 
+    /**
+     * Executes the print action by printing out a message.
+     */
     public void execute()
     {
         System.out.println("This action is a print action");
     }
 
+    /**
+     * toString method for the PrintAction class.
+     *
+     * @return string saying that this is the PrintAction class
+     */
     public String toString()
     {
         return "PrintAction";
