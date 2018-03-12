887c10ee75c43d12b12f5c664f68277de60c82d5
David <david@Davids-MacBook-Pro.local>
2018-03-12 12:37:18 -0400

    will perform the same tests from Text save on FSM save

--- src/test/java/testfiles/TestingSaveAndLoad.java
+++ src/test/java/testfiles/TestingSaveAndLoad.java
@@ -2,36 +2,51 @@ package test.java.testfiles;
 
 import proj2.document.Document;
 import proj2.filehandler.concretefilehandler.TextSave;
+import proj2.filehandler.concretefilehandler.FSMSave;
+
 import java.io.*;
 import proj2.document.Action;
 import proj2.document.actions.*;
 
 public class TestingSaveAndLoad{
-	Document d;
-	TextSave f;
+	Document d1;
+	Document d2;
+	TextSave text;
+	FSMSave fsm;
 	public TestingSaveAndLoad()
 	{
 		 
-		d= new Document();
-	    f= new TextSave(d);
+		d1= new Document();
+		d2= new Document();
+	    text= new TextSave(d1);
+	    fsm= new FSMSave(d2);
 	    Action print=new PrintAction();
 	    Action none= new NoAction();
 		Action sound=new SoundAction();
-		d= new Document();
-		f= new TextSave(d);
-		d.addState("A",10,52);
-		d.addAction("A",none);
-		d.addState("B",55,117);
-		d.addAction("B",sound);
-		d.addState("C",1,27);
-		d.addAction("C",print);
-		d.addTransition("A","B","z");
-		d.addTransition("C","A","r");
-		d.addTransition("A","A","s");
-		d.addTransition("B","A","q");
-		d.toggleAccept("A");
 		
-
+		d1.addState("A",10,52);
+		d1.addAction("A",none);
+		d1.addState("B",55,117);
+		d1.addAction("B",sound);
+		d1.addState("C",1,27);
+		d1.addAction("C",print);
+		d1.addTransition("A","B","z");
+		d1.addTransition("C","A","r");
+		d1.addTransition("A","A","s");
+		d1.addTransition("B","A","q");
+		d1.toggleAccept("A");
+		
+		d2.addState("A",10,52);
+		d2.addAction("A",none);
+		d2.addState("B",55,117);
+		d2.addAction("B",sound);
+		d2.addState("C",1,27);
+		d2.addAction("C",print);
+		d2.addTransition("A","B","z");
+		d2.addTransition("C","A","r");
+		d2.addTransition("A","A","s");
+		d2.addTransition("B","A","q");
+		d2.toggleAccept("A");
 
 
 
@@ -41,17 +56,20 @@ public class TestingSaveAndLoad{
 
 	public void save()
 	{
-		f.save("FiniteStateMachine");
+		text.save("FiniteStateMachine");
+		FSMSave.save("FiniteStateMachine");
 	}
 
 	public void load()
 	{
-		f.load("FiniteStateMachine");
+		text.load("FiniteStateMachine");
+		FSMSave.load("FiniteStateMachine");
 	}
 
 	public void save2()
 	{
-		f.save("FiniteStateMachine_copy");
+		text.save("FiniteStateMachine_copy");
+		FSMSave.save("FiniteStateMachine_copy");
 	}
 
 	public static void main(String[] args) throws FileNotFoundException
